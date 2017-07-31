20f6fcde39e70800eb407540f5eff157cc92de59
Šarūnas Navickas <sarunas.navickas@adform.com>
2017-07-31 14:56:58 +0300

    Move forward with actual agent

--- oshino_prometheus/agent.py
+++ oshino_prometheus/agent.py
@@ -1,5 +1,32 @@
+import aiohttp
+
 from oshino import Agent
 
+from oshino_prometheus.utils import parse_prometheus_metrics
 
-class StubAgent(Agent):
-    pass
+
+class PrometheusAgent(Agent):
+
+    def endpoints(self):
+        return self._data["endpoints"]
+
+    async def get_raw_metrics(self, url):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                return await resp.text()
+
+    async def process(self, event_fn):
+        logger = self.get_logger()
+
+        for endpoint in endpoints:
+            raw = await self.get_raw_metrics(endpoint)
+            metrics = parse_prometheus_metrics(raw)
+
+            for key, metric in metrics.items():
+                logger.debug("Received metric: {0}".format(metric))
+                val, meta = metric
+                service = self.prefix + key
+                event_fn(metric_f=val,
+                         state="ok",
+                         service=service,
+                         attrs=meta)
